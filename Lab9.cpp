63e3e07a27f80b9f636b0fa8a260d2c5066aed04
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2024-06-09 11:39:07 +0300

    Add files via upload

---
+++ Lab9.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string str = "Hello, world! How are you?";
+    std::string підрядок = "world";
+
+    // Знаходження останнього входження "світ" в рядку
+    size_t позиція = str.rfind(підрядок);
+
+    if (позиція != std::string::npos) {
+        std::cout << "Last entry \"" << підрядок << "\" found in position: " << позиція << std::endl;
+    }
+    else {
+        std::cout << "No substring found." << std::endl;
+    }
+
+    return 0;
+}
